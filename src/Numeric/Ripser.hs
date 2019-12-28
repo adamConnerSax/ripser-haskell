1350896bc44d33569fe5eceab0aaf07eb1d4cd28
adamConnerSax <adam_conner_sax@yahoo.com>
2019-12-28 14:56:47 -0500

    Working

--- src/Numeric/Ripser.hs
+++ src/Numeric/Ripser.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 module Numeric.Ripser
-  ()
+  ( Input(..)
+  , Dimension
+  , Threshold
+  , Ratio
+  , Distance
+  , PersistenceInterval
+  , callRipser
+  , lowerDistanceMatrixFromFile
+  , distanceMatrix
+  )
 where
 
 import qualified System.Process.Typed          as SP
@@ -15,6 +25,7 @@ import           Data.Maybe                     ( fromMaybe
                                                 , maybe
                                                 )
 import           Data.Void                      ( Void )
+import qualified Data.Scientific               as Sci
 import qualified Data.Text                     as T
 import qualified Data.Text.Lazy                as TL
 import qualified Data.Text.Lazy.Encoding       as TL
@@ -26,18 +37,23 @@ import           Control.Concurrent.STM         ( atomically )
 import qualified Text.Megaparsec               as P
 import qualified Text.Megaparsec.Byte          as P
 import qualified Text.Megaparsec.Byte.Lexer    as PL
+import           Data.Word                      ( Word8 )
 
 import qualified Persistence.Filtration        as PH
 import qualified Control.Applicative           as A
+import           Control.Applicative            ( (<|>) )
 
-data Input = LowerDistance (LA.Matrix Double) | PointCloud (LA.Matrix Double) | Sparse [(Int, Int, Double)]
+import           Math.NumberTheory.Powers.Squares
+                                                ( exactSquareRoot )
+
+data Input = LowerDistance (LA.Matrix Double) | PointCloud (LA.Matrix Double) | Sparse [(Int, Int, Double)] deriving (Show)
 type Dimension = Int -- specify max dimension for computed persistent homology
 type Threshold = Double -- only compute Rips complexes up to this diameter
 type Ratio = Double -- only report persistence pairs with death/birth > Ratio
 
 type Distance a = (a -> a -> Double) -- should be >= 0 for all a
 
-data PersistenceInterval = PersistenceInterval { dim :: Int, interval :: PH.BarCode Double }
+data PersistenceInterval = PersistenceInterval { dim :: Int, interval :: PH.BarCode Double } deriving (Show)
 
 callRipser
   :: Maybe FilePath
@@ -56,14 +72,20 @@ callRipser pathM dimM threshM ratioM input = do
         LowerDistance _ -> "lower-distance"
         PointCloud    _ -> "point-cloud"
         Sparse        _ -> "sparse"
-      opts    = fmap T.unpack [dimOpt, threshOpt, ratioOpt, inputOpt]
-      encoded = TL.encodeUtf8 $ TL.fromStrict $ encodeInput input
+      opts = fmap T.unpack [dimOpt, threshOpt, ratioOpt, inputOpt]
+  putStrLn $ "input: " ++ show input
+  let encodedText = encodeInput input
+  putStrLn $ "Encoded input: " ++ T.unpack encodedText
+  let encodedBS = TL.encodeUtf8 $ TL.fromStrict $ encodeInput input
       ripserProc =
-        SP.setStdin (SP.byteStringInput encoded)
+        SP.setStdin SP.createPipe --(SP.byteStringInput encodedBS)
           $ SP.setStdout SP.byteStringOutput
-          $ SP.proc ripserPath opts
+          $ SP.proc ripserPath [] --opts
   SP.withProcessWait_ ripserProc $ \p -> do
+    BL.hPutStr (SP.getStdin p) encodedBS
+    S.hClose (SP.getStdin p)
     unparsed <- atomically (SP.getStdout p)
+    putStr $ "UnParsed output:\n" ++ show unparsed
     case parseOutput unparsed of
       Left parseErr ->
         X.throwIO $ S.userError "parse failure on output of ripser."
@@ -74,7 +96,7 @@ encodeInput (LowerDistance mLD) =
   let (rows, cols) = LA.size mLD
   in  T.intercalate " " $ fmap (T.pack . show) $ do
         c <- [0 .. (cols - 1)]
-        r <- [c .. (rows - 1)]
+        r <- [(c + 1) .. (rows - 1)]
         return $ mLD `LA.atIndex` (r, c)
 
 encodeInput (PointCloud mPC) =
@@ -87,35 +109,45 @@ encodeInput (Sparse lSP) = T.intercalate "\n" $ fmap
   )
   lSP
 
-type Parser a = P.Parsec Void BL.ByteString
+type Parser a = P.Parsec Void BL.ByteString a
+
+void :: Functor f => f a -> f ()
+void = fmap (const ())
+
+space = PL.space P.space1 A.empty A.empty
+
+lexeme = PL.lexeme space
+
+trimLeading :: Parser ()
+trimLeading = PL.space P.space1 A.empty A.empty -- spaces
+
+ignoreLine :: Parser ()
+ignoreLine = P.manyTill (P.takeWhileP Nothing (const True)) P.eol >> return ()
+
+ignoreLinesUntil :: Parser a -> Parser ()
+ignoreLinesUntil p = void $ P.manyTill ignoreLine (P.lookAhead p)
 
 parseOutput :: BL.ByteString -> Either T.Text [PersistenceInterval]
-parseOutput ro = either (T.pack . P.errorBundlePretty) id
-  $ P.runParser p "ripser-output" ro
+parseOutput ro =
+  either (Left . T.pack . P.errorBundlePretty) Right
+    $ P.runParser p "ripser-output" ro
  where
-  ignoreLine :: Parser ()
-  ignoreLine = P.manyTill (P.takeWhileP Nothing (const True)) eol >> return ()
-  trimLeading :: Parser ()
-  trimLeading = PL.space P.space1 A.empty A.empty -- spaces
   parseDimLine :: Parser Int
   parseDimLine = do
     void $ P.string "persistence intervals in dim "
-    PL.lexeme PL.decimal
-    trimLeading
+    dim <- lexeme PL.decimal
     void $ P.string ":"
-    eol
+    P.eol
+    return dim
   parseBarCode :: Parser (PH.BarCode Double)
   parseBarCode = do
     void $ P.string "["
-    birth <- PL.signed (return ()) PL.float
-    trimLeading
-    void $ P.string "["
-    trimLeading
-    death <- PL.signed (return ()) PL.float
-    trimLeading
+    birth <- lexeme $ PL.signed (return ()) PL.float
+    void $ P.string ","
+    death <- lexeme $ PL.signed (return ()) PL.float
     void $ P.string ")"
     return $ (birth, PH.Finite death)
-  parseBarCodes :: Parser [PH.BarCode]
+  parseBarCodes :: Parser [PH.BarCode Double]
   parseBarCodes = A.some parseOne
    where
     parseOne = do
@@ -123,17 +155,56 @@ parseOutput ro = either (T.pack . P.errorBundlePretty) id
       bc <- parseBarCode
       P.eol
       return bc
-  parseDim :: [PersistenceInterval]
+  parseDim :: Parser [PersistenceInterval]
   parseDim = do
     d        <- parseDimLine
     barCodes <- parseBarCodes
     return $ fmap (PersistenceInterval d) barCodes
   p :: Parser [PersistenceInterval]
   p = do
-    dim <- P.manyTill ignoreLine (P.lookAhead parseDimLine)
+    ignoreLinesUntil parseDimLine
     concat <$> A.some parseDim
 
+toWord8 :: Char -> Word8
+toWord8 = toEnum . fromEnum
 
+nonNumber :: Parser ()
+nonNumber = PL.space
+  (void $ A.some $ P.noneOf
+    ( fmap toWord8
+    $ ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.', 'e', 'E']
+    )
+  )
+  A.empty
+  A.empty
+
+numbers :: Parser [Double]
+numbers = fmap (fmap Sci.toRealFloat) $ (trimLeading <|> void P.eol) >> A.many
+  (PL.lexeme nonNumber (PL.signed (return ()) PL.scientific))
+
+lowerDistanceMatrixFromFile :: FilePath -> IO Input
+lowerDistanceMatrixFromFile fp = do
+  asBS <- BL.readFile fp
+--  putStrLn $ show asBS
+  nums <- case P.runParser numbers fp asBS of
+    Left errB -> X.throwIO $ S.userError
+      (  "parse failure on lower distance matrix file: "
+      ++ P.errorBundlePretty errB
+      )
+    Right x -> return x
+  let n = L.length nums
+  putStrLn $ "lowerDistanceMatrixFromFile: Loaded " ++ show n ++ " numbers"
+  nCols <- case exactSquareRoot ((8 * n) + 1) of
+    Nothing -> X.throwIO $ S.userError
+      ("Loaded " ++ show n ++ " bumbers from file but that is not triangular!")
+    Just r -> return $ ((r - 1) `div` 2) + 1 -- +1 here because we have only below diagonal
+  putStrLn $ "lowerDistanceMatrixFromFile: LDM has " ++ show nCols ++ " columns"
+  let indices = do
+        col <- [0 .. (nCols - 1)]
+        row <- [(col + 1) .. (nCols - 1)]
+        return (row, col)
+      mLD = LA.assoc (nCols, nCols) 0 $ zip indices nums
+  return $ LowerDistance mLD
 
 
 distanceMatrix
---
+++ test/ripserTest.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import qualified Numeric.Ripser                as R
+import qualified Data.List                     as L
+import qualified System.IO                     as S
+-- this assumes ripser directory, with working executable, and examples dir
+-- is located at "ripser"
+
+main :: IO ()
+main = do
+  S.hSetBuffering S.stdout S.NoBuffering
+  let pathToRipser   = "ripser/ripser"
+      pathToExamples = "ripser/examples/"
+  random16LD <-
+    R.lowerDistanceMatrixFromFile
+    $  pathToExamples
+    ++ "random16.lower_distance_matrix"
+  putStrLn $ "loaded input: \n" ++ show random16LD
+{-  sphere_3_192LD <-
+    R.lowerDistanceMatrixFromFile
+    $  pathToExamples
+    ++ "sphere_3_192.lower_distance_matrix"
+  putStrLn $ "loaded input: \n" ++ show sphere_3_192LD -}
+  ripserOutput <- R.callRipser (Just pathToRipser)
+                               Nothing
+                               Nothing
+                               Nothing
+                               random16LD
+  putStrLn $ "Output:\n" ++ (L.intercalate "\n" $ fmap show ripserOutput)
+
